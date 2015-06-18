34f16eabfdefd6df1adbb86f249ebc88952511f8
Scott <scotthadzik@weber.edu>
2015-06-17 18:44:33 -0600

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('heliDashboardApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
